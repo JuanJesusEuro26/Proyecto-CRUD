fff82e07dadb4e734644f75cb059cf61c3fa8726
JuanJesusEuro26 <juan.jesus.sanchez@eurotransportcar.com>
2026-03-18 10:01:05 +0100

    EMPEZAMOS CON EL MENU CLIENTES

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -4,7 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use AppBundle\Repository\UsersRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller{
     /**
@@ -20,10 +22,57 @@ class IndexController extends Controller{
     function cargarRegistroAction(){
         return $this->render("proyectfiles/registro.html.twig");
     }
+
+    /**
+     * @Route("/Index/Identificacion", name="Identificacion")
+     */
+    function identificarUserAction(Request $request){
+        $email=$request->request->get('email');
+        $password=$request->request->get('pswd');
+
+         /** @var UsersRepository $repo */
+         $repo=$this->getDoctrine()->getRepository('AppBundle:Usuario');
+       
+        $existeemail=$repo->comprobarEmail($email);
+        if($existeemail){ //Comprobamos email
+            //Comprobamos si el usuario esta activo (el usuario no ha borrado la cuenta)
+            $activo=$repo->isuseractive($email);
+            if($activo==1){
+                $contraseñacorrecta=$repo->comprobarContraseña($email, $password);
+                if($contraseñacorrecta){
+                    $rolusuario=$repo->comprobarRol($email);
+                    if($rolusuario==1){ //Si es admin
+                        return new JsonResponse(array("redirect" => $this->generateUrl('admin_home')));
+                    } else if($rolusuario==2){ //Si es cliente
+                        return new JsonResponse(array("redirect" => $this->generateUrl('client_home')));
+                    } else{ //Si no tiene rol
+                        return new JsonResponse(array("error"=>"Este usuario no tiene rol asignado. Contacte con el soporte."));
+                    }
+                } else{
+                    return new JsonResponse(array("error"=>"Este email esta registrado pero la contraseña introducida es incorrecta."));
+                }
+            } else if ($activo==0){
+                return new JsonResponse(array("error"=>"Este usuario ya ha sido registrado pero la cuenta ha sido borrada.")); //EL PROCEDIMIENTO PARA CUANDO PASA ESTO TENGO QUE PREGUNTARLO
+            }
+        } else{
+            //El email no esta registrado por lo tanto da respuesta json
+            return new JsonResponse(array("error"=>"Este email no se ha encontrado en nuestra base de datos."));
+        }
+    } 
+
+
+    /**
+     * @Route("/Index/Admin", name="admin_home")
+     */
+    public function adminHomeAction() {
+        return $this->render('proyectfiles/adminmenu.html.twig');
+    }
+
+    /**
+     * @Route("/Index/Cliente", name="client_home")
+     */
+    public function clientHomeAction() {
+        return $this->render('proyectfiles/clientmenu.html.twig');
+    }
 }
-
-
-
-
-
 ?>
--- src/AppBundle/Repository/UsersRepository.php
+++ src/AppBundle/Repository/UsersRepository.php
@@ -8,12 +8,11 @@ use AppBundle\Entity\Usuario;
 class UsersRepository extends EntityRepository{
 
     public function crearUsers(Usuario $usuario){
-        $conn= $this->getEntityManager()->getConnection();
 
         $sql = "INSERT INTO registrar_users (id, is_active, password, email, fecha_nacimiento, nombre, num_operaciones, rol_id) 
                 VALUES (:id, :active, :pass, :email, :fecha, :nombre, :ops, :rol_id)";
 
-        $stmnt=$conn->prepare($sql);
+        $stmnt=$this->prepareconn()->prepare($sql);
         $stmnt->bindValue('id', $usuario->getID());
         $stmnt->bindValue('active', $usuario->getActive());
         $stmnt->bindValue('pass', $usuario->getcontraseña());
@@ -28,26 +27,24 @@ class UsersRepository extends EntityRepository{
     }
 
     public function comprobarEmail(string $email){
-        $conn= $this->getEntityManager()->getConnection();
 
         $sql = "SELECT email FROM registrar_users WHERE email = :correo";
 
-        $stmnt=$conn->prepare($sql);
+        $stmnt=$this->prepareconn()->prepare($sql);
         $stmnt->bindValue('correo', $email);
         $stmnt->execute();
 
-        return $stmnt->fetch(); //Devolvemos el resultado. Si no hay nada devuelve false
+        return $stmnt->fetch(); //Devolvemos el resultado. Si este email no esta registrado devuelve false
     }
 
     public function generateID(){
-        $conn= $this->getEntityManager()->getConnection();
         $nuevoID=0;
         $existe=true;
 
         while($existe){
             $nuevoID= mt_rand(1000,9999);
             $sql = "SELECT id FROM registrar_users WHERE id = :id_buscado";
-            $stmnt=$conn->prepare($sql);
+            $stmnt=$this->prepareconn()->prepare($sql);
             $stmnt->bindValue('id_buscado', $nuevoID);
             $stmnt->execute();
 
@@ -57,6 +54,57 @@ class UsersRepository extends EntityRepository{
         }
 
         return $nuevoID;
+    }
+
+    public function comprobarRol(string $email){
+        $sql="SELECT rol_id FROM registrar_users WHERE email= :correo";
+        $stmnt=$this->prepareconn()->prepare($sql);
+        $stmnt->bindValue('correo', $email);
+        $stmnt->execute();
+
+        $rolfila=$stmnt->fetch();
+        $rol=$rolfila['rol_id'];
+        if($rol==false){
+            return false; //El usuario no tiene rol (flujo de trabajo inusual, todos los usuarios se crean con rol)
+        } else {
+            return $rol;
+        }
+    }
+
+    public function comprobarContraseña(string $email, string $contraseña){
+        $sql="SELECT password FROM registrar_users WHERE email= :correo";
+        $stmnt=$this->prepareconn()->prepare($sql);
+        $stmnt->bindValue('correo', $email);
+        $stmnt->execute();
+
+        $fila=$stmnt->fetch(); //Guardamos en fila la informacion de la fila extraida (fetch no saca un solo valor, saca un array con formato fila=["password"=>"1234"])
+        if(!$fila){
+            return false; //El usuario no tiene contraseña (flujo de trabajo inusual, todos los usuarios se crean con contraseña)
+        }
+
+        $hashGuardado=$fila['password']; //Accedemos a la contraseña de la fila hash
+
+        // password_verify compara el texto plano con el hash
+        return password_verify($contraseña, $hashGuardado);
+    }
+
+    private function prepareconn(){
+        $conn= $this->getEntityManager()->getConnection();
+        return $conn;
+    }
+
+    public function isuseractive(string $email){
+        $sql="SELECT is_active FROM registrar_users WHERE email= :correo";
+        $stmnt=$this->prepareconn()->prepare($sql);
+        $stmnt->bindValue('correo', $email);
+        $stmnt->execute();
+
+        $fila=$stmnt->fetch();
+        if(!$fila){
+            return false; 
+        }
+        $activo=$fila['is_active'];
+        return $activo;
     }
 }
 
